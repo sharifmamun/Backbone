25b2947c45fbeaf8e4bb039d7198d358846091de
S.M. Al Mamun <sharif.mamun@gmail.com>
2014-11-16 01:38:57 -0800

    Added basic funcionalities

--- js/views/app.js
+++ js/views/app.js
@@ -7,6 +7,14 @@ app.AppView = Backbone.View.extend({
 	// of the app already present in the HTML
 	el: '#todoapp',
 
+	// New
+	// Delegated events for creating new items, and clearing completed ones.
+	events: {
+		'keypress #new-todo': 'createOnEnter',
+		'click #clear-completed': 'clearCompleted',
+		'click #toggle-all': 'toggleAllComplete'
+	},
+
 	// Our template for the line of statistics at the bottom of the app.
 	statsTemplate: _.template( $('#stats-template').html() ),
 
@@ -20,12 +28,45 @@ app.AppView = Backbone.View.extend({
 
 		this.listenTo(app.Todos, 'add', this.addOne);
 		this.listenTo(app.Todos, 'reset', this.addAll);
+
+		this.listenTo(app.Todos, 'change:completed', this.filterOne);
+		this.listenTo(app.Todos, 'filter', this.filterAll);
+		this.listenTo(app.Todos, 'all', this.render);
+
+		app.Todos.fetch();
+	},
+
+	// Rendering the app just means refreshing the statistics -- the rest
+	// of the app doesn't change
+	render: function() {
+		var completed = app.Todos.completed().length;
+		var remaining = app.Todos.remaining().length;
+
+		if ( app.Todos.length ) {
+			this.$main.show();
+			this.$footer.show();
+
+			this.$footer.html(this.statsTemplate ({
+				completed: completed,
+				remaining: remaining
+			}));
+
+			this.$('#filters li a')
+				.removeClass('selected')
+				.filter('[href="#/' + ( app.TodoFilter || '') + '"]')
+				.addClass('selected');				
+		} else {
+			this.$main.hide();
+			this.$footer.hide();
+		}
+
+		this.allCheckbox.checked = !remaining;
 	},
 
 	// Add a single todo item to the list by creating a view for it, and 
 	// appending it's element to the `<ul>`
 	addOne: function( todo ) {
-		var view = new TodoView( { model: todo } );
+		var view = new app.TodoView( { model: todo } );
 		$('#todo-list').append( view.render().el );
 	},
 
@@ -33,5 +74,44 @@ app.AppView = Backbone.View.extend({
 	addAll: function() {
 		this.$('#todo-list').html('');
 		app.Todos.each(this.addOne, this);
+	},
+
+	filterOne: function() {
+		todo.trigger('visible');		
+	},
+
+	filterAll: function() {
+		app.Todos.each(this.filterOne, this);
+	},
+
+	newAttributes: function() {
+		return {
+			title: this.$input.val().trim(),
+			order: app.Todos.nextOrder(),
+			completed: false
+		};
+	},
+
+	// If you hit return in the main input field, create new Todo model,
+	// persisting it to localStorage.
+	createOnEnter: function() {
+		if ( event.which !== ENTER_KEY  || !this.$input.val().trim() ) {
+			return;
+		}
+
+		app.Todos.create( this.newAttributes() );
+		this.$input.val('');
+	},
+
+	// Clear all completed todo items and destroying their models.
+	clearCompleted: function() {
+		var completed = this.allCheckbox.checked;
+
+		app.Todos.each( function( todo ) {
+			todo.save({
+				'completed': completed
+			});
+		});
 	}
+
 });
